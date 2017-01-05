5944ea4ba43c3411ba9ab8dcc5e71c3f69ce3e27
Anatoliy Kmetyuk <anatoliykmetyuk@gmail.com>
2017-01-05 18:17:49 +0200

    Rudimentary corpus mining

--- src/main/scala/analysis/Main.scala
+++ src/main/scala/analysis/Main.scala
@@ -1,22 +1,67 @@
 package analysis
 
-import scala.collection.JavaConversions._
+import scala.collection.JavaConverters._
+
+import fs2._
 
 import org.jsoup.Jsoup
+import org.jsoup.nodes.{Document, Element}
+
 
 object Main {
-  lazy val indexUrl = "http://stage48.net/studio48/lyricsindex.html"
-  lazy val index = Jsoup.connect(indexUrl).get()
+  implicit val strategy = Strategy.fromFixedDaemonPool(8)
+  implicit class StreamableSeq[A](x: Seq[A]) { def asStream[F[_]]: Stream[F, A] = Stream[F, A](x: _*) }
+  implicit class FilterableStream[F[_], A](x: Stream[F, A]) { def withFilter(f: A => Boolean): Stream[F, A] = x.filter(f) }
+
+  val indexUrl = "http://stage48.net/studio48/lyricsindex.html"
+  val urlResolutionAttempts = 10
+
+  def get(url: String): Stream[Task, Document] = {
+    @annotation.tailrec def loop(i: Int): Either[Throwable, Document] =
+      try Right { Jsoup.connect(url).get() }
+      catch { case t: Throwable => println(s"Exception occurred: $t.getMessage"); if (i > 0) loop(i - 1) else Left(t) }
+    
+    Stream.eval(Task { loop(urlResolutionAttempts) }).flatMap {
+      case Right(doc) => Stream.emit(doc)
+      case Left (_  ) => Stream.empty[Task, Document]
+    }
+  }
   
-  lazy val links = for {
-    container <- index    .getElementsByClass("two-columns")
-    link      <- container.getElementsByTag  ("a")
+  val links: Pipe[Task, Document, (String, String)] = s => for {
+    index     <- s
+    container <- index    .getElementsByClass("two-columns").asScala.asStream
+    link      <- container.getElementsByTag  ("a").asScala.asStream
 
     relativePath = link.attr("href")
-    text         = link.text
+    title        = link.text
+    if relativePath.nonEmpty && title.nonEmpty
+    fullPath     = s"http://stage48.net/studio48/$relativePath"    
+  } yield (fullPath, title)
 
-    if relativePath.nonEmpty && text.nonEmpty
-  } yield Url(relativePath, text)
+  val songs: Pipe[Task, (String, String), Song] = s => for {
+    (path, title) <- s
+    songPage      <- get(path)
+    container     <- songPage .getElementsByClass("two-columns").asScala.asStream
+    columns        = container.getElementsByClass("column").asScala
+  } yield {
+    println(s"Done processing $title")
+
+    def processColumn(c: Element): List[String] = c.getElementsByTag("p").asScala.map(_.text).toList
+    val kanjiVerses       = processColumn(columns(0))
+    val romajiVerses      = processColumn(columns(1))
+    val translationVerses = processColumn(columns(2))
+
+    Song(title, kanjiVerses, romajiVerses, translationVerses)
+  }
+
+  val songsStream: Stream[Task, Song] = get(indexUrl)
+    .through(links)
+    .through(songs)
 }
 
-case class Url(link: String, text: String)
+case class Song(
+  title       : String
+, kanji       : List[String]
+, romaji      : List[String]
+, translation : List[String]
+)
